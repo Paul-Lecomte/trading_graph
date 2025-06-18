cd00a39426ad5369824b03a35fc9979db3030e47
Paul <lecomtepaulvd@gmail.com>
2025-06-18 07:14:35 +0200

    new file

---
+++ components/Chart/DrawingTools.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "DrawingTools"
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
