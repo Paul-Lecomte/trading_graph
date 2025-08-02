37c380b34e630971d15ef3be3fb663acac9b917b
Paul <lecomtepaulvd@gmail.com>
2025-08-02 15:04:57 +0200

    comment

--- components/Chart/IndicatorPanel.vue
+++ components/Chart/IndicatorPanel.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 import {defineComponent} from 'vue'
-
+// This component is a placeholder for the Indicator Panel in the Chart application
 export default defineComponent({
   name: "IndicatorPanel"
 })
